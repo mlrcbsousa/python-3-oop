8c2b3911a9d7b5a0e1924c5e0d52ecb152215841
Manuel Sousa <mlrcbsousa@gmail.com>
2024-04-20 14:44:01 +0200

    Add ex01 S1E7.py

--- ex00/tester.py
+++ ex00/tester.py
@@ -2,23 +2,24 @@ from S1E9 import Stark, Character
 
 
 def main():
-    Ned = Stark("Ned")
-    print(Ned.__dict__)
-    print(Ned.is_alive)
-    Ned.die()
-    print(Ned.is_alive)
-
-    print(Ned.__doc__)
-    print(Ned.__init__.__doc__)
-    print(Ned.die.__doc__)
-
-    print("---")
-    Lyanna = Stark("Lyanna", False)
-    print(Lyanna.__dict__)
-
     try:
+        Ned = Stark("Ned")
+        print(Ned.__dict__)
+        print(Ned.is_alive)
+        Ned.die()
+        print(Ned.is_alive)
+
+        print(Ned.__doc__)
+        print(Ned.__init__.__doc__)
+        print(Ned.die.__doc__)
+
+        print("---")
+        Lyanna = Stark("Lyanna", False)
+        print(Lyanna.__dict__)
+
         Character("Hodor")
-    except TypeError as e:
+
+    except Exception as e:
         print(e)
 
 
